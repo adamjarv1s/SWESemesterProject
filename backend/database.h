cb7d76a39f82dbb20f9a2c5485a66e0b045b73b1
AbbyBuchholz <abuchholz1@ufl.edu>
2026-03-11 16:08:14 -0400

    Corrected database working.

--- backend/database.h
+++ backend/database.h
@@ -1,8 +1,9 @@
 #ifndef DATABASE_H
 #define DATABASE_H
+#include <string>
 
 int databaseInit(int argc, char *argv[]);
 // todo:
-std::string logPeriod(int user, std::string currentDate, std::string startDate, int heaviness, bool lastDay);
+//std::string logPeriod(int user, std::string currentDate, std::string startDate, int heaviness, bool lastDay);
 //string getAccountType
 #endif
