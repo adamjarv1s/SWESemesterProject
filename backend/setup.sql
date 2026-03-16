c29865144abad47a3682deeac4ce1b86fc6d7ee3
AbbyBuchholz <abuchholz1@ufl.edu>
2026-03-16 14:10:07 -0400

    Fixed database set up, creation of utilities.cpp, and createUser functionality. (#15)
    * Database on VScode
    
    * Fixed some database set up, added a utilities.cpp for general functions, and I got createUser working.

--- backend/setup.sql
+++ backend/setup.sql
@@ -1,4 +1,4 @@
-drop schema uterusdata;
+drop schema if exists uterusdata;
 CREATE DATABASE IF NOT EXISTS uterusdata;
 USE uterusdata;
 create table IF NOT EXISTS UserInfo(
@@ -6,7 +6,8 @@ create table IF NOT EXISTS UserInfo(
                                        Name VARCHAR(100) NOT NULL,
                                        Pet VARCHAR(100) NOT NULL,
                                        Type INT NOT NULL,
-                                       Streak INT NOT NULL
+                                       Streak INT NOT NULL,
+                                       lastDay DATE NOT NULL
 );
 create table IF NOT EXISTS periodData(
                                          id INT AUTO_INCREMENT PRIMARY KEY,
@@ -14,16 +15,12 @@ create table IF NOT EXISTS periodData(
                                          StartDate DATE NOT NULL,
                                          CurrentLength int NOT NULL,
                                          Heaviness int not null,
-                                         LastDay bool
+                                         LastDay bool not null,
+                                         FOREIGN KEY (id) REFERENCES UserInfo(id)
 );
 create table if not exists purchaseData(
                                            id INT AUTO_INCREMENT PRIMARY KEY,
                                            currentDiamonds int not null,
-                                           purchasedItem float
+                                           purchasedItem float,
+                                           FOREIGN KEY (id) REFERENCES UserInfo(id)
 );
-create table if not exists userLoginData
-(
-    id       INT AUTO_INCREMENT PRIMARY KEY,
-    username varchar(100) not null,
-    password varchar(100) not null
-)
---
+++ backend/utilities.h
@@ -0,0 +1,9 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+#include <iostream>
+#include <ctime>
+#include <string>
+
+std::string getCurrentDate();
+
+#endif
