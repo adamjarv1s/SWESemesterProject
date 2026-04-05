1d03cba9f79f532d20f90f1273bc8c3edf02eb42
Adam Jarvis <pokemaster0425@gmail.com>
2026-04-05 16:57:51 -0400

    Added more specific irregularity functions

--- backend/CycleMath.h
+++ backend/CycleMath.h
@@ -12,5 +12,11 @@ bool shouldBleedingStartingtoday(int day, int year, int lastStart, double averag
 bool inFertilityWindow(int day, int year, int lastStart, double averageCycle);
 bool checkMissed(int day, int year, int lastStart, double averageCycle);
 bool checkIrregular(int day, int year, int lastStart, double averageCycle, std::vector<std::pair<int,int>> periods);
-
+bool checkLastCycleUnder21(int day, int year, int lastStart, double averageCycle, bool notConcernedFlag);
+bool checkLastThreeUnder21(std::vector<std::pair<int,int>> periods, bool notConcernedFlag);
+bool checkLastCycleOver35(int day, int year, int lastStart, double averageCycle, bool notConcernedFlag);
+bool checkLastThreeOver35(std::vector<std::pair<int,int>> periods, bool notConcernedFlag);
+bool checkBleedingAfterStopped(int day, int year, int bleedingEndDay, bool notConcernedFlag);
+bool checkBleedingFor7Days(int day, int year, int startDay, bool notConcernedFlag);
+bool checkNoPeriodIn90Days(int day, int year, int lastStart, bool notConcernedFlag);
 #endif
